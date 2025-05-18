5c94ffb7249231bbffa912a00febeee73ef2d4cb
Maximviktorovic2305 <maximviktorovic123@ya.ru>
2025-05-18 11:07:56 +1000

    add: добавил получение всех калькуляций

--- server/main.go
+++ server/main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/Knetic/govaluate"
-	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -39,73 +38,6 @@ func getCalculations(c echo.Context) error {
 	return c.JSON(http.StatusOK, calculations)
 }
 
-func postCalculations(c echo.Context) error {
-	var req CalculationRequest
-	if err := c.Bind(&req); err != nil {
-		fmt.Println("Error binding request:", err)
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid Request"}) // Ошибка запроса
-	}
-
-	fmt.Println("Received expression:", req.Expression)
-
-	result, err := calculateExpression(req.Expression)
-	if err != nil {
-		fmt.Println("Error calculating expression:", err)
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid Expression"}) // Ошибка вычислений
-	}
-
-	calc := Calculation{
-		ID:         uuid.NewString(),
-		Expression: req.Expression,
-		Result:     result,
-	}
-
-	calculations = append(calculations, calc)
-
-	fmt.Println("Calculation added:", calc)
-
-	return c.JSON(http.StatusCreated, calc)
-}
-
-func patchCalculations(c echo.Context) error {
-	id := c.Param("id")
-
-	var req CalculationRequest
-	if err := c.Bind(&req); err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid Request"}) // Ошибка запроса
-	}
-
-	result, err := calculateExpression(req.Expression)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid expression"})
-	}
-
-	for i, calculation := range calculations {
-		if calculation.ID == id {
-			calculations[i].Expression = req.Expression
-			calculations[i].Result = result
-			return c.JSON(http.StatusOK, calculations[i])
-		}
-
-	}
-
-	return c.JSON(http.StatusBadRequest, map[string]string{"error": "Calculation not found"})
-
-}
-
-func deleteCalculation(c echo.Context) error {
-	id := c.Param("id")
-
-	for i, calculation := range calculations {
-		if calculation.ID == id {
-			calculations = append(calculations[:i], calculations[i+1:]...)
-			return c.NoContent(http.StatusNoContent)
-		}
-	}
-
-	return c.JSON(http.StatusBadRequest, map[string]string{"error": "Calculation not found"})
-}
-
 func main() {
 	e := echo.New()
 
@@ -113,9 +45,6 @@ func main() {
 	e.Use(middleware.Logger())
 
 	e.GET("/calculations", getCalculations)
-	e.POST("/calculations", postCalculations)
-	e.PATCH("/calculations/:id", patchCalculations)
-	e.DELETE("/calculations/:id", deleteCalculation)
 
 	fmt.Println("Server is running on http://localhost:8080")
 	e.Start("localhost:8080")
